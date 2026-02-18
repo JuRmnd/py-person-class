2fce0c624be70d454774e63802ae98063dba923a
JuRmnd <remond.neiluj@gmail.com>
2026-02-18 12:05:48 +0000

    solution

--- app/main.py
+++ app/main.py
@@ -1,22 +1,21 @@
 class Person:
-    people = []
+    people = {}
 
     def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
+        self.people[name] = self
 
 
 def create_person_list(people: list) -> list:
     new_persons = [Person(person["name"], person["age"]) for person in people]
     for person in people:
         new_person = list(filter((lambda n_person: n_person.name == person["name"]), new_persons))[0]
-        if "wife" in person.keys() and person["wife"]:
-            wife = list(filter((lambda n_person: n_person.name == person["wife"]), new_persons))[0]
+        if "wife" in person.keys() and person.get("wife"):
+            wife = list(filter((lambda n_person: n_person.name == person.get("wife")), new_persons))[0]
             new_person.wife = wife
-        if "husband" in person.keys() and person["husband"]:
-            husband = list(filter((lambda n_person: n_person.name == person["husband"]), new_persons))[0]
+        if "husband" in person.keys() and person.get("husband"):
+            husband = list(filter((lambda n_person: n_person.name == person.get("husband")), new_persons))[0]
             new_person.husband = husband
-
-    Person.people = new_persons
 
     return new_persons
